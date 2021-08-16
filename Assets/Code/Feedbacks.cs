cc79571516cfb4283e215e231192e6250267e010
Jack Walker <jackmw94@gmail.com>
2021-08-16 18:57:07 +0100

    Added feel feedbacks and "cool visual effects" assets.

---
+++ Assets/Code/Feedbacks.cs
@@ -0,0 +1,23 @@
+﻿using MoreMountains.Feedbacks;
+using UnityEngine;
+using UnityExtras.Code.Optional.Singletons;
+
+namespace Code
+{
+    public class Feedbacks : SingletonMonoBehaviour<Feedbacks>
+    {
+        [SerializeField] private MMFeedback _orbiterHitsPlayer;
+
+        public void SetTriggerOrbiterHitsPlayerFeedbackActive(bool active)
+        {
+            if (!active)
+            {
+                _orbiterHitsPlayer.Stop(Vector3.zero);
+            }
+            else
+            {
+                _orbiterHitsPlayer.Play(Vector3.zero);
+            }
+        }
+    }
+}
