5db3ac6a961261dd093dd777f3dc7bd9bc16b5d7
Jack Walker <jackmw94@gmail.com>
2021-08-17 16:18:51 +0100

    Removed Feel feedbacks plugin to facilitate windows builds

--- Assets/Code/Level/Player/PlayerHealth.cs
+++ Assets/Code/Level/Player/PlayerHealth.cs
@@ -5,12 +5,6 @@ namespace Code.Level.Player
 {
     public class PlayerHealth : EntityHealth
     {
-        protected override void OnHitTaken()
-        {
-            base.OnHitTaken();
-            Feedbacks.Instance.TriggerHealthLostFeedback();
-        }
-
         protected override bool DoesObjectDamageUs(GameObject gameObj)
         {
 #if UNITY_EDITOR
