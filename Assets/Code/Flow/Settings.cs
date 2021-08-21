0cdae863d5613483a1203acb9f0adc219fecd931
Jack Walker <jackmw94@gmail.com>
2021-08-21 01:25:56 +0100

    Reset level + update remote config buttons

--- Assets/Code/Flow/Settings.cs
+++ Assets/Code/Flow/Settings.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using Code.Core;
 using Code.Level;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityExtras.Code.Core;
@@ -9,11 +10,16 @@ namespace Code.Flow
 {
     public class Settings : MonoBehaviour
     {
+        private const string UpdateRemoteConfigDefaultText = "Update game configuration";
+        
         [SerializeField] private Button _toggleSettingsButton;
         [SerializeField] private CanvasGroup _canvasGroup;
         [SerializeField] private float _toggleDuration = 0.25f;
         [Space(15)]
         [SerializeField] private Button _nextLevelButton;
+        [SerializeField] private Button _resetLevelButton;
+        [SerializeField] private Button _updateRemoteConfigButton;
+        [SerializeField] private TextMeshProUGUI _updateRemoteConfigLabel;
 
         private bool _settingsOn = false;
         private Coroutine _turnOnOffCoroutine = null;
@@ -22,6 +28,8 @@ namespace Code.Flow
         {
             _toggleSettingsButton.onClick.AddListener(SettingsButtonClicked);
             _nextLevelButton.onClick.AddListener(NextLevelButtonListener);
+            _resetLevelButton.onClick.AddListener(ResetLevelButtonListener);
+            _updateRemoteConfigButton.onClick.AddListener(UpdateRemoteConfigButtonListener);
 
             TurnOffInstant();
         }
@@ -30,6 +38,13 @@ namespace Code.Flow
         {
             _toggleSettingsButton.onClick.RemoveListener(SettingsButtonClicked);
             _nextLevelButton.onClick.RemoveListener(NextLevelButtonListener);
+            _resetLevelButton.onClick.RemoveListener(ResetLevelButtonListener);
+            _updateRemoteConfigButton.onClick.RemoveListener(UpdateRemoteConfigButtonListener);
+        }
+
+        private void OnSettingShowing()
+        {
+            _updateRemoteConfigLabel.text = UpdateRemoteConfigDefaultText;
         }
 
         private void SettingsButtonClicked()
@@ -46,8 +61,31 @@ namespace Code.Flow
             SettingsButtonClicked();
         }
 
+        private void ResetLevelButtonListener()
+        {
+            GameContainer gameContainer = GameContainer.Instance;
+            LevelManager levelManager = gameContainer.LevelManager;
+            levelManager.ResetCurrentLevel();
+            SettingsButtonClicked();
+        }
+
+        private void UpdateRemoteConfigButtonListener()
+        {
+            _updateRemoteConfigButton.interactable = false;
+            RemoteConfigHelper.RequestRefresh(success =>
+            {
+                _updateRemoteConfigButton.interactable = true;
+                _updateRemoteConfigLabel.text = $"{UpdateRemoteConfigDefaultText} - was {(success ? "successful" : "unsuccessful")}";
+            });
+        }
+
         private void TurnSettingsOnOff(bool on)
         {
+            if (on)
+            {
+                OnSettingShowing();
+            }
+            
             if (_turnOnOffCoroutine != null)
             {
                 StopCoroutine(_turnOnOffCoroutine);
