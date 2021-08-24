29e1e11cab6429aadfe6ff7597a06747b8a750ca
Jack Walker <jackmw94@gmail.com>
2021-08-24 22:02:55 +0100

    Refactored build validation so it can be ran outside of builds

--- Assets/Code/Debugging/CircumBuildValidation.cs
+++ Assets/Code/Debugging/CircumBuildValidation.cs
@@ -1,11 +1,7 @@
 ﻿#if UNITY_EDITOR
 
-using System.Collections.Generic;
-using System.Linq;
-using UnityEngine;
 using UnityEditor.Build;
 using UnityEditor.Build.Reporting;
-using UnityExtras.Code.Core;
 
 namespace Code.Debugging
 {
@@ -15,8 +11,7 @@ namespace Code.Debugging
 
         public void OnPreprocessBuild(BuildReport report)
         {
-            IEnumerable<IValidateable> validateables = Object.FindObjectsOfType<MonoBehaviour>().OfType<IValidateable>();
-            validateables.ApplyFunction(v => v.Validate());
+            CircumTools.RunValidation();
         }
     }
 }
---
+++ Assets/Code/Debugging/CircumTools.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Code.Level.Player;
+using UnityEditor;
+using UnityEngine;
+using UnityExtras.Code.Core;
+
+namespace Code.Debugging
+{
+    public static class CircumTools
+    {
+#if UNITY_EDITOR
+        [MenuItem("Circum/Reset saved player stats")]
+        public static void ResetSavedPlayerStats()
+        {
+            PlayerStats.ResetSavedPlayerStats();
+        }
+        
+        [MenuItem("Circum/Run validation")]
+        public static void RunValidation()
+        {
+            IEnumerable<IValidateable> validateables = Object.FindObjectsOfType<MonoBehaviour>().OfType<IValidateable>();
+            validateables.ApplyFunction(v => v.Validate());
+            CircumDebug.Log("--- Validation complete ---");
+        }
+#endif
+    }
+}
