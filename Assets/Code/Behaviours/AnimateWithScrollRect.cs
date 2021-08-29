681b2028ee7042d1e0ab6e8155c1474416dd45ac
Jack Walker <jackmw94@gmail.com>
2021-08-29 15:56:54 +0100

    Inter level UI background animation

---
+++ Assets/Code/Behaviours/AnimateWithScrollRect.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityExtras.Code.Optional.EasingFunctions;
+
+namespace Code.Behaviours
+{
+    public class AnimateWithScrollRect : MonoBehaviour
+    {
+        [SerializeField] private bool _invert;
+        [SerializeField] private string _animatorProperty = "NormalisedTime";
+        [SerializeField] private ScrollRect _scrollRect;
+        [SerializeField] private Animator _animator;
+
+        private int _animatorPropertyHash;
+
+        private void Awake()
+        {
+            _animatorPropertyHash = Animator.StringToHash(_animatorProperty);
+        }
+
+        private void Update()
+        {
+            float clampedNormalisedPosition = Mathf.Clamp01(_scrollRect.verticalNormalizedPosition);
+            float easedNormalisedPosition = EasingFunctions.EaseInOutSine(clampedNormalisedPosition);
+            float reducedNormalisedPosition = Mathf.Lerp(0.05f, 0.95f, easedNormalisedPosition);
+            reducedNormalisedPosition = _invert ? 1f - reducedNormalisedPosition : reducedNormalisedPosition;
+            
+            _animator.SetFloat(_animatorPropertyHash, reducedNormalisedPosition);
+        }
+    }
+}
