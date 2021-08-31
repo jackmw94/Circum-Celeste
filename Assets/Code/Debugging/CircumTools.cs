f2498ba71137970f36ef7f57695e224346649822
Jack Walker <jackmw94@gmail.com>
2021-08-31 15:24:49 +0100

    Localisation with auto translations

--- Assets/Code/Debugging/CircumTools.cs
+++ Assets/Code/Debugging/CircumTools.cs
@@ -1,9 +1,14 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using Code.Level.Player;
+using Lean.Localization;
+using Lean.Localization.Editor;
 using UnityEditor;
-using UnityEngine;
 using UnityExtras.Code.Core;
+
+#if UNITY_EDITOR
+using UnityEngine;
+#endif
 
 namespace Code.Debugging
 {
@@ -34,6 +39,71 @@ namespace Code.Debugging
             IEnumerable<IValidateable> validateables = Object.FindObjectsOfType<MonoBehaviour>().OfType<IValidateable>();
             validateables.ApplyFunction(v => v.Validate());
             CircumDebug.Log("--- Validation complete ---");
+        }
+
+        [MenuItem("Circum/Translate/Auto translate all")]
+        public static void AutoTranslateAll()
+        {
+            AutoTranslateScene(true);
+        }
+
+        [MenuItem("Circum/Translate/Auto translate new")]
+        public static void AutoTranslateNew()
+        {
+            AutoTranslateScene(false);
+        }
+
+        private static void AutoTranslateScene(bool force)
+        {
+            LeanPhrase[] phrases = Object.FindObjectsOfType<LeanPhrase>();
+            LeanLocalization localisation = Object.FindObjectOfType<LeanLocalization>();
+            LeanLanguage[] languages = localisation.Prefabs.Select(p => (p.Root as GameObject)?.GetComponent<LeanLanguage>()).ToArray();
+
+            AutoTranslatePhrases(phrases, languages, force);
+        }
+        
+        private static void AutoTranslatePhrases(LeanPhrase[] phrases, LeanLanguage[] languages, bool force)
+        {
+            string englishLanguageName = "English";
+            
+            foreach (LeanPhrase phrase in phrases)
+            {
+                LeanPhrase.Entry english = phrase.Entries.FirstOrDefault(p => p.Language.EqualsIgnoreCase(englishLanguageName));
+                if (english == null)
+                {
+                    CircumDebug.Log($"Cannot find entry for english for {phrase.gameObject}");
+                    english = phrase.AddEntry(englishLanguageName, phrase.gameObject.name);
+                }
+
+                english.Text = phrase.name;
+
+                string textOutput = default;
+                string languageCodeInput = "en";
+                
+                Undo.RecordObject(phrase, "Auto Translate");
+
+                foreach (LeanLanguage leanLanguage in languages)
+                {
+                    if (leanLanguage.name.EqualsIgnoreCase(englishLanguageName))
+                    {
+                        continue;
+                    }
+                    
+                    LeanPhrase.Entry entry = phrase.Entries.FirstOrDefault(p => p.Language.EqualsIgnoreCase(leanLanguage.name)) ?? phrase.AddEntry(leanLanguage.name, phrase.name);
+
+                    if (string.IsNullOrEmpty(entry.Text) && !force)
+                    {
+                        continue;
+                    }
+                    
+                    if (LeanPhrase_Editor.TryAutoTranslate(languageCodeInput, leanLanguage.TranslationCode, english.Text, ref textOutput))
+                    {
+                        entry.Text = textOutput;
+                    }
+                }
+                
+                EditorUtility.SetDirty(phrase);
+            }
         }
 #endif
     }
---
+++ Assets/Code/UI/LeanLocalisedTextMeshProUGUI.cs
@@ -0,0 +1,47 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace Lean.Localization
+{
+    /// <summary>This component will update a UI.Text component with localized text, or use a fallback if none is found.</summary>
+    [ExecuteInEditMode]
+    [DisallowMultipleComponent]
+    [RequireComponent(typeof(TextMeshProUGUI))]
+    [AddComponentMenu(LeanLocalization.ComponentPathPrefix + "Localized TextMeshProUGUI")]
+    public class LeanLocalisedTextMeshProUGUI : LeanLocalizedBehaviour
+    {
+        [Tooltip("If PhraseName couldn't be found, this text will be used.")]
+        public string FallbackText;
+
+        // This gets called every time the translation needs updating
+        public override void UpdateTranslation(LeanTranslation translation)
+        {
+            // Get the Text component attached to this GameObject
+            TextMeshProUGUI text = GetComponent<TextMeshProUGUI>();
+
+            // Use translation?
+            if (translation != null && translation.Data is string)
+            {
+                text.text = LeanTranslation.FormatText((string)translation.Data, text.text, this, gameObject);
+            }
+            // Use fallback?
+            else
+            {
+                text.text = LeanTranslation.FormatText(FallbackText, text.text, this, gameObject);
+            }
+        }
+
+        protected virtual void Awake()
+        {
+            // Should we set FallbackText?
+            if (string.IsNullOrEmpty(FallbackText))
+            {
+                // Get the Text component attached to this GameObject
+                TextMeshProUGUI text = GetComponent<TextMeshProUGUI>();
+
+                // Copy current text to fallback
+                FallbackText = text.text;
+            }
+        }
+    }
+}
