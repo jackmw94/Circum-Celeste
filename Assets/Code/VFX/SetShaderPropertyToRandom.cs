255e3d1d9a405a05a0f05f301e3486d067992e65
Jack Walker <jackmw94@gmail.com>
2021-09-17 18:16:38 +0100

    Attempted fix for blackhole dissipate visual glitch. Added blackhole shader time offset to allow for variation

---
+++ Assets/Code/VFX/SetShaderPropertyToRandom.cs
@@ -0,0 +1,20 @@
+﻿using Code.Behaviours;
+using UnityEngine;
+
+namespace Code.VFX
+{
+    public class SetShaderPropertyToRandom : MonoBehaviour
+    {
+        [SerializeField] private MaterialProvider _materialProvider;
+        [SerializeField] private string _materialPropertyName;
+        [SerializeField] private float _minValue;
+        [SerializeField] private float _maxValue;
+        
+        private void Awake()
+        {
+            Material mat = _materialProvider.GetMaterial();
+            float randomValue = Random.Range(_minValue, _maxValue);
+            mat.SetFloat(_materialPropertyName, randomValue);
+        }
+    }
+}
