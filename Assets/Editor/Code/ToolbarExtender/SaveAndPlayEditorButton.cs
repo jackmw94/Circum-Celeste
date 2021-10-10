1ad80fa6920cdbd4cb640f0902b3e22ba52752fb
Jack Walker <jackmw94@gmail.com>
2021-10-10 22:52:23 +0100

    Added toolbar extender for play from boot button next to play button

---
+++ Assets/Editor/Code/ToolbarExtender/SaveAndPlayEditorButton.cs
@@ -0,0 +1,62 @@
+﻿using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace ToolbarExtender
+{
+    [InitializeOnLoad]
+    public class SaveAndPlayEditorButton
+    {
+        private const string PlayerPrefsPreviousScenePath = "Circum_PrevOpenScene";
+        private static readonly Texture _texture;
+    
+        static SaveAndPlayEditorButton()
+        {
+            _texture = EditorGUIUtility.IconContent(@"winbtn_mac_inact").image;
+            ToolbarExtender.LeftToolbarGUI.Add(OnToolbarGUI);
+
+            EditorApplication.playModeStateChanged += ChangingPlayModeState;
+        }
+
+        static void ChangingPlayModeState(PlayModeStateChange stateChange)
+        {
+            if (stateChange != PlayModeStateChange.EnteredEditMode || !PlayerPrefs.HasKey(PlayerPrefsPreviousScenePath))
+            {
+                return;
+            }
+        
+            string scenePath = PlayerPrefs.GetString(PlayerPrefsPreviousScenePath);
+            if (string.IsNullOrEmpty(scenePath))
+            {
+                return;
+            }
+        
+            PlayerPrefs.SetString(PlayerPrefsPreviousScenePath, "");
+            EditorSceneManager.OpenScene(scenePath);
+        }
+
+        static void OnToolbarGUI()
+        {
+            GUILayout.Space(225);
+
+            GUI.enabled = !EditorApplication.isPlaying;
+
+            if (GUILayout.Button(new GUIContent(_texture, "Play from Boot"), ToolbarStyles.s_commandButtonStyle))
+            {
+                var currentScene = SceneManager.GetActiveScene();
+                PlayerPrefs.SetString(PlayerPrefsPreviousScenePath, currentScene.path);
+
+                for (int i = 0; i < SceneManager.sceneCount; i++)
+                {
+                    Scene scene = SceneManager.GetSceneAt(i);
+                    EditorSceneManager.MarkSceneDirty(scene);
+                    EditorSceneManager.SaveScene(scene);
+                }
+
+                EditorSceneManager.OpenScene(EditorBuildSettings.scenes[0].path, OpenSceneMode.Single);
+                EditorApplication.EnterPlaymode();
+            }
+        }
+    }
+}
---
+++ Assets/Editor/Code/ToolbarExtender/ToolbarStyles.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+namespace ToolbarExtender
+{
+    static class ToolbarStyles
+    {
+        public static readonly GUIStyle s_commandButtonStyle;
+        public static readonly GUIStyle s_testButtonStyle;
+        public static readonly GUIStyle s_missionDropDownStyle;
+
+        static ToolbarStyles()
+        {
+            s_commandButtonStyle = new GUIStyle( "Command" )
+            {
+                fontSize = 16,
+                alignment = TextAnchor.MiddleCenter,
+                imagePosition = ImagePosition.ImageAbove,
+                fontStyle = FontStyle.Bold
+            };
+        
+            s_testButtonStyle = new GUIStyle( "Command" )
+            {
+                fontSize = 12,
+                alignment = TextAnchor.MiddleCenter,
+                imagePosition = ImagePosition.ImageAbove,
+                fontStyle = FontStyle.Normal,
+                fixedWidth = 140,
+            };
+
+            s_missionDropDownStyle = new GUIStyle( "toolbarDropDown" )
+            {
+                fontSize = 12,
+                alignment = TextAnchor.MiddleLeft,
+                imagePosition = ImagePosition.TextOnly,
+                fontStyle = FontStyle.Normal,
+                fixedWidth = 180,
+            };
+        }
+    }
+}
