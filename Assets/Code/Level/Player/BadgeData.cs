5b961d7259dd3678f0194675c906af32e6f06887
Jack Walker <jackmw94@gmail.com>
2021-10-19 21:38:38 +0100

    Removed badge state enum since we now support all states of the two bools

--- Assets/Code/Level/Player/BadgeData.cs
+++ Assets/Code/Level/Player/BadgeData.cs
@@ -1,22 +1,9 @@
-﻿using System;
-
-namespace Code.Level.Player
+﻿namespace Code.Level.Player
 {
     public struct BadgeData
     {
-        // todo: change from bools to badge state; fewer variables and no invalid states
-        public BadgeState BadgeState { get; set; }
         public bool IsPerfect { get; set; }
         public bool HasGoldTime { get; set; }
         public bool HasPerfectGoldTime { get; set; }
-    }
-
-    [Flags]
-    public enum BadgeState
-    {
-        None = 0,
-        Perfect = 1 << 0,
-        GoldTime = 1 << 1,
-        PerfectGoldTime = Perfect | GoldTime
     }
 }
