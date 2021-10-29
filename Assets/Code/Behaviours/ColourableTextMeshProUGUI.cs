19513f5af3ceb69887b8d83d775b8a536acdf434
Jack Walker <jackmw94@gmail.com>
2021-10-29 18:31:38 +0100

    Rotate colour scripts and a colourable component for tmpro text

---
+++ Assets/Code/Behaviours/ColourableTextMeshProUGUI.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics;
+using TMPro;
+using UnityEngine;
+
+namespace Code.Behaviours
+{
+    public class ColourableTextMeshProUGUI : Colourable
+    {
+        [SerializeField] private TextMeshProUGUI _textMeshProUGUI;
+
+        [Conditional("UNITY_EDITOR")]
+        private void OnValidate()
+        {
+            if (!_textMeshProUGUI)
+            {
+                _textMeshProUGUI = GetComponent<TextMeshProUGUI>();
+            }
+        }
+
+
+        public override Color GetColour()
+        {
+            return _textMeshProUGUI.color;
+        }
+
+        public override void SetColour(Color colour)
+        {
+            _textMeshProUGUI.color = colour;
+        }
+    }
+}
---
+++ Assets/Code/Behaviours/RotateColour.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace Code.Behaviours
+{
+    public class RotateColour : MonoBehaviour
+    {
+        [SerializeField] private Color _disabledColour;
+        [SerializeField] private Colourable _colourable;
+        [SerializeField] private float _speed = 1f;
+        [SerializeField] private float _intensity = 1f;
+
+        private void OnDisable()
+        {
+            _colourable.SetColour(_disabledColour);
+        }
+
+        private void Update()
+        {
+            float red = Mathf.PerlinNoise(Time.time * _speed, 0f);
+            float green = Mathf.PerlinNoise(Time.time * _speed, 10000f);
+            float blue = Mathf.PerlinNoise(Time.time * _speed, 50000f);
+        
+            Color colour = new Color(red, green, blue) * _intensity;
+            _colourable.SetColour(colour);
+        }
+    }
+}
