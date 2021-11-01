52fd2928f27ed2dd967f2784b9411e03df76ad92
Jack Walker <jackmw94@gmail.com>
2021-11-01 15:51:06 +0000

    Added a social button to settings menu

---
+++ Assets/Code/UI/LinkButton.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Diagnostics;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Code.UI
+{
+    public class LinkButton : MonoBehaviour
+    {
+        [SerializeField] private Button _linkButton;
+        [SerializeField] private string _linkUrl;
+
+        [Conditional("UNITY_EDITOR")]
+        private void OnValidate()
+        {
+            if (!_linkButton)
+            {
+                _linkButton = GetComponent<Button>();
+            }
+        }
+
+        private void Awake()
+        {
+            _linkButton.onClick.AddListener(LinkButtonClicked);
+        }
+
+        private void OnDestroy()
+        {
+            _linkButton.onClick.RemoveListener(LinkButtonClicked);
+        }
+
+        private void LinkButtonClicked()
+        {
+            Application.OpenURL(_linkUrl);
+        }
+    }
+}
